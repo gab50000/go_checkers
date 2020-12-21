987d0fe63cbf83db1e6186b2d4afae2dd1d31ba6
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-12-21 16:50:02 +0100

    Update moves

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type playerColor int
+type direction int
 
 const (
 	white playerColor = iota
 	black
 )
 
+const (
+	up direction = iota
+	down
+)
+
+// Position of a token
 type Position struct {
 	i int
 	j int
 }
 
+// Move holding start and end position
 type Move struct {
 	from Position
 	to   Position
@@ -75,6 +85,116 @@ func getPositions(color playerColor, board *[8][8]string) []Position {
 		}
 	}
 	return positions
+}
+
+func getMoves(color playerColor, dir direction, board *[8][8]string) []Move {
+	moves := make([]Move, 0)
+
+	positions := getPositions(color, board)
+
+	for _, pos := range positions {
+		moves = append(moves, getManMoves(color, dir, pos, board)...)
+	}
+	return moves
+}
+
+func getManMoves(color playerColor, dir direction, pos Position, board *[8][8]string) []Move {
+	moves := make([]Move, 0, 2)
+	i, j := pos.i, pos.j
+	var ii int
+	switch {
+	case dir == up && i > 0:
+		ii = i - 1
+	case dir == down && i < 7:
+		ii = i + 1
+	default:
+		return []Move{}
+	}
+	for dj := -1; dj <= 1; dj += 2 {
+		jj := j + dj
+		if jj < 0 || jj > 7 {
+			continue
+		}
+
+		if board[ii][jj] == "" {
+			moves = append(moves, Move{pos, Position{ii, jj}})
+		}
+	}
+
+	return moves
+}
+
+func oppositeColor(color playerColor) playerColor {
+	var oppColor playerColor
+	switch color {
+	case white:
+		oppColor = black
+	case black:
+		oppColor = white
+	}
+	return oppColor
+}
+
+func getManJumps(
+	color playerColor,
+	dir direction,
+	pos Position,
+	board *[8][8]string) []Move {
+
+	moves := make([]Move, 0)
+	i, j := pos.i, pos.j
+
+	enemyColor := oppositeColor(color)
+	var enemyPrefix string
+	switch enemyColor {
+	case black:
+		enemyPrefix = "B"
+	case white:
+		enemyPrefix = "W"
+	}
+
+	var iEnemy, iDestination, jEnemy, jDestination int
+	switch {
+	case dir == up && i > 1:
+		{
+			iEnemy = i - 1
+			iDestination = i - 2
+		}
+	case dir == down && i < 6:
+		{
+			iEnemy = i + 1
+			iDestination = i + 2
+		}
+	default:
+		return []Move{}
+	}
+
+	for dj := -1; dj <= 1; dj += 2 {
+		jEnemy = j + dj
+		if jEnemy == 0 || jEnemy == 7 {
+			continue
+		}
+		jDestination = j + 2*dj
+
+		enemyElem := board[iEnemy][jEnemy]
+		if enemyElem != "" &&
+			enemyElem[:1] == enemyPrefix &&
+			board[iDestination][jDestination] == "" {
+			moves = append(moves, Move{pos, Position{iDestination, jDestination}})
+		}
+	}
+	return moves
+}
+
+// func getKingMoves(color playerColor, dir direction, pos Position) {}
+
+// func getKingJumps(color playerColor, dir direction, pos Position) {}
+
+func getJumps(color playerColor, dir direction, board *[8][8]string) []Move {
+	moves := make([]Move, 0)
+	// positions := getPositions(color, board)
+
+	return moves
 }
 
 func main() {
