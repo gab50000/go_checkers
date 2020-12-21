cf7622a50baa5d187b5bc535fefb7b997c23c9fa
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-12-21 16:49:57 +0100

    Update tests

--- game_test.go
+++ game_test.go
@@ -4,6 +4,48 @@ import (
 	"testing"
 )
 
-func TestSth(t *testing.T) {
+func TestManMoves(t *testing.T) {
+	board := [8][8]string{}
+	board[3][3] = "BM"
+
+	moves := getMoves(black, down, &board)
+
+	if len(moves) != 2 {
+		t.Errorf("len(moves) is %d, should be 2", len(moves))
+	}
+
+	board = [8][8]string{}
+	board[3][0] = "BM"
+
+	moves = getMoves(black, down, &board)
+	if len(moves) != 1 {
+		t.Errorf("len(moves) is %d, should be 1", len(moves))
+	}
+
+	board = getBoard()
+	moves = getMoves(white, up, &board)
+
+	if len(moves) != 7 {
+		t.Errorf("len(moves) is %d, should be 7", len(moves))
+	}
+}
+
+func TestManJumps(t *testing.T) {
+	board := [8][8]string{}
+	board[3][3] = "BM"
+	board[4][4] = "WM"
+
+	jumps := getManJumps(white, up, Position{4, 4}, &board)
+
+	if len(jumps) != 1 {
+		t.Errorf("length(jumps) == %d, should be 1", len(jumps))
+	}
+
+	jump := jumps[0]
+
+	target := Move{Position{4, 4}, Position{2, 2}}
+	if jump != target {
+		t.Error("Expected", target, "got", jump)
+	}
 
 }
